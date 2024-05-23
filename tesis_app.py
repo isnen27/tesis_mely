bec00432a205f7e439aa84591cff7c738ffb802f
Isnen Hadi <89633584+isnen27@users.noreply.github.com>
2024-05-23 19:01:03 +0700

    Update tesis_app.py

--- tesis_app.py
+++ tesis_app.py
@@ -19,7 +19,7 @@ from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from nltk.stem import PorterStemmer
-import nltk.tokenize.punkt
+from nltk.data import find
 
 # for providing path
 import os
@@ -65,6 +65,14 @@ def load_data3():
     df3 = pd.read_csv('output_stemmed_with_8_labels.csv')
     return df3
 df3 = load_data3()
+
+
+
+nltk.data.path.append('./nltk_data')
+try:
+    find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir='./nltk_data')
 
 def main(df):
     # Main Page Design
